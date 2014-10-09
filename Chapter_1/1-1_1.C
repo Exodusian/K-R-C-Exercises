0582c282801df5a0ab2e3a3391c4e06ce044796f
Exodusian <exodusauron@hotmail.com>
2014-10-09 09:11:26 -0500

    Update and rename 1-1_1.C to 1-1_1.c

--- Chapter_1/1-1_1.C
+++
@@ -1,8 +0,0 @@
-/* Exercise 1-1: Run the Hello World program on your system. 
- * See what compiler errors you get when you leave things out.*/
- 
-#include <stdio.h>
- 
-int main(void) {
-  printf("Hello, World!);
-}
---
+++ Chapter_1/1-1_1.c
@@ -0,0 +1,10 @@
+/* 
+ * Exercise 1-1: Run the Hello World program on your system. Experiment with
+ * leaving parts out of the program, to see what error messages you get.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello, World!);
+}
