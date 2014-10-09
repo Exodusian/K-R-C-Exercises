76c6c5f557bf018533ab29316fc3bfe1659ee533
Exodusian <exodusauron@hotmail.com>
2014-10-09 09:12:12 -0500

    Update 1-1_1.c

--- Chapter_1/1-1_1.c
+++ Chapter_1/1-1_1.c
@@ -1,5 +1,5 @@
 /* 
- * Exercise 1-1: Run the Hello World program on your system. Experiment with
+ * Exercise 1-1: Run the "hello world" program on your system. Experiment with
  * leaving parts out of the program, to see what error messages you get.
  */
 
