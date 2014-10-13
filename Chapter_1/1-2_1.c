b481156cdb4872dba8dc5f0d3afd24025e1e3761
Exodusian <exodusauron@hotmail.com>
2014-10-13 13:45:22 -0500

    Update 1-2_1.c

--- Chapter_1/1-2_1.c
+++ Chapter_1/1-2_1.c
@@ -1,4 +1,5 @@
-/* Exercise 1-2: Experiment to find out what happens when printf's argument
+/*
+ *Exercise 1-2: Experiment to find out what happens when printf's argument
  *string contains \c, where c is some character not listed above.
  */
 
