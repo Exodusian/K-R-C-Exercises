db6982fc45554b0ca4a77f1a720790a9270db82a
Exodusian <exodusauron@hotmail.com>
2014-10-14 14:02:29 -0500

    Create 1-4.c

---
+++ Chapter_1/1-4.c
@@ -0,0 +1,28 @@
+/*
+ * Exercise 1-4: Write a program to print the corresponding
+ * Celsius to Fahrenheit table.
+ */
+
+
+#include <stdio.h>
+
+/*
+ * print Celsius-Fahrenheit table
+ * for celsius = 0, 20, ..., 300; floating point version
+ */
+
+int main(void) {
+        float celsius, fahr;
+        int lower, upper, step;
+  
+        lower = 0;
+        upper = 300;
+        step = 20;
+        
+        celsius = lower;
+        while (celsius <= upper) {
+                fahr = ((celsius*5.0) / 9.0) + 32;
+                printf("%3.0f %6.1f\n", celsius, fahr);
+                celsius = celsius + step;
+        }
+}
